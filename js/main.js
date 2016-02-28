7f89499cdde3109f1b3e486113ccc68a7437331c
juntaoshuai <595979939@qq.com>
2016-02-28 22:52:06 +0800

    header page

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by shuai on 2016/2/28.
+ */
+seajs.config({
+    // 别名配置
+    alias: {
+        'jquery': './sea-modules/jquery/jquery-1.8.0.min.js',
+        'layer':'./sea-modules/layer-v2.1/layer.js'
+    }
+});
+
+define(function(require,exports,module){
+    var $=require('jquery');
+       //菜单展开收缩
+       $(".main_left li h3").click(function(){
+
+           $(this).find("span").toggleClass("arrow-down").parents("h3").next("dl").slideToggle();
+       });
+
+});
+
+
+
