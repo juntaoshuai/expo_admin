da12d25d3667c7a5d461c5890e3dafd4ff540e52
juntaoshui <595979939@qq.com>
2016-03-10 18:42:49 +0800

    3/10 18:42

--- js/main.js
+++ js/main.js
@@ -60,6 +60,61 @@ $(function(){
     $.delPopup("#delPic","此图片已在展位中，您确定要删除吗？");
 //当前页面打开一个新窗口
 
+    (function(){
+        //增加产品分组
+      var i=2;
+        $("#add_proGroup_btn").click(function(){
+            i++==20?$(this).hide():"";
+
+           var html='<li class="mt10"><label class="f14 c000">产品分组: </label><input type="text" value="输入分组名"><a href="add_product.html" target="_blank" class="btn_grey92 ml20">添加产品</a><a href="javascript:;" class="remove-group">移除分组</a></li>';
+           $(html).appendTo($(this).prev());
+
+        });
+        $(".remove-group").live('click',function(){
+            $(this).closest("li").remove();
+        })
+    })();
+
+    (function(){
+        //清空文本框提示文字和输入文字变黑色
+        jQuery.clearTxt=function(obj){
+            $(obj).focus(function(){
+                    if($(this).val()==this.defaultValue){
+                        $(this).val("");
+                    }
+                })
+                .blur(function(){
+                    if($(this).val()==""){
+                        $(this).css("color","#999").val(this.defaultValue);
+                    }
+                });
+        }
+        $.clearTxt(".txt");
+        $.clearTxt(" .group-list input");
+
+        jQuery.chageFontColor=function(obj){
+            $(obj).on("input propertychange",function(){
+                // alert(1242);
+                $(this).css("color","#000");
+            });
+        }
+        $.chageFontColor(".txt");
+        $.chageFontColor(".group-list input");
+
+    })();
+//地址当为中国以外地区，则省份隐藏
+    $("#country").change(function(){
+        if($(this).val()==-1){
+            $("#province").hide();
+        }else{
+            $("#province").show();
+        }
+    });
+
+//展位基本设置时，保存用户选择视频还是图集
+
+
+
 
 
 
